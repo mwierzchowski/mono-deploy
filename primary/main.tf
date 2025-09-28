b3246b2fee042fa8468f7e2682ee2f2213ac2975
Marcin Wierzchowski <mwierzch@gmail.com>
2025-09-28 20:23:29 +0200

    Refactor Terraform configuration to improve tag management and add Log Analytics workspace

--- primary/main.tf
+++ primary/main.tf
@@ -3,29 +3,33 @@ resource "random_id" "suffix" {
 }
 
 locals {
-  family     = "mono"
-  repo       = "${local.family}-deploy"
-  env        = "primary"
-  uniqueName = "${local.family}${random_id.suffix.hex}"
-  tags = {
+  family      = "mono"
+  env         = "primary"
+  unique_name = "${local.family}${random_id.suffix.hex}"
+  tags_shared = {
+    env     = local.env
+    repo    = "${local.family}-deploy"
+    service = "shared"
+  }
+  tags_jvm = {
     env  = local.env
-    repo = local.repo
+    repo = "${local.family}-jvm"
   }
 }
 
 resource "azurerm_resource_group" "rg" {
   name     = "rg-${local.family}-${local.env}"
   location = "northeurope"
-  tags     = { repo = local.repo }
+  tags     = local.tags_shared
 }
 
 resource "azurerm_storage_account" "sa" {
-  name                     = local.uniqueName
+  name                     = local.unique_name
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
-  tags                     = merge(local.tags, { service = "shared" })
+  tags                     = local.tags_shared
 }
 
 resource "azurerm_storage_container" "tfstate" {
@@ -35,20 +39,42 @@ resource "azurerm_storage_container" "tfstate" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = local.uniqueName
+  name                = local.unique_name
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   sku                 = "Basic"
   admin_enabled       = false
-  tags                = merge(local.tags, { service = "shared" })
+  tags                = local.tags_shared
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+resource "azurerm_log_analytics_workspace" "la" {
+  name                = "log-analytics"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  sku                 = "PerGB2018"
+  retention_in_days   = 30
+  tags                = local.tags_shared
 }
 
-output "storage_account_name" {
-  value = azurerm_storage_account.sa.name
+resource "azurerm_container_app_environment" "apps_env" {
+  name                       = "container-apps"
+  location                   = azurerm_resource_group.rg.location
+  resource_group_name        = azurerm_resource_group.rg.name
+  log_analytics_workspace_id = azurerm_log_analytics_workspace.la.id
+  tags                       = local.tags_shared
+}
+
+resource "azurerm_user_assigned_identity" "acr_pull" {
+  name                = "uami-acr-pull"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  tags                = local.tags_shared
+}
+
+resource "azurerm_role_assignment" "acr_pull" {
+  scope                = azurerm_container_registry.acr.id
+  role_definition_name = "AcrPull"
+  principal_id         = azurerm_user_assigned_identity.acr_pull.principal_id
 }
 
 output "acr_login_server" {

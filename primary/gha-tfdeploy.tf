2524d2b631f831b14ab636964923a9af1d78b88f
Marcin Wierzchowski <mwierzch@gmail.com>
2025-10-11 09:59:27 +0200

    Add ACA workflow and improve deployment (#3)
    * Rename job `Deploy` to `ACA` in `app-aca.yaml` workflow for improved clarity.
    
    * Update `primary.yaml` workflow and Terraform configuration
    
    - Remove unused Git configuration step from `.github/workflows/primary.yaml`.
    - Update `deploy_github_ref` in `gha-tfdeploy.tf` to reference `aca-workflow` branch.
    
    * Add federated credentials for main and feature branches in Terraform config
    
    - Create separate `azuread_application_federated_identity_credential` resources for `main` and `feature` branches.
    - Update `local` values to define GitHub ref variables for both branches.
    - Add `lifecycle` block to ensure safe resource updates.
    
    * Update `app-aca.yaml` to enhance dispatch workflow with inputs, env variables, and Terraform validation steps
    
    * chore(app-aca): upsert smerek2 → image=mono058f3201.azurecr.io/mono-jvm/smerek:3, repo=mono-jvm2
    
    * Refactor `app-aca.yaml` workflow to simplify inputs, enhance YAML updates, and improve Terraform validation steps
    
    * Update `app-aca.yaml` workflow to set empty default for `repository` input and add commented reference placeholder
    
    * Upsert smerek5 configuration
    
    * Enhance `app-aca.yaml` workflow with additional deployment link formatting in step summary
    
    * Fix `repository` input reference in `app-aca.yaml` workflow for correct variable usage
    
    * Upsert smerek6 configuration
    
    * Refactor `app-aca.yaml` workflow to streamline inputs, enhance variable usage, and simplify deployment steps
    
    * Update `CONFIG_FILE` path in `app-aca.yaml` workflow to use explicit `primary` directory reference
    
    * Remove unused `MONO_DEPLOY_PUSH_TOKEN` secret from `app-aca.yaml` workflow.
    
    * Remove unused `permissions` block from `app-aca.yaml` workflow.
    
    * Upsert smerek configuration
    
    * Rename `app-aca.yaml` to `.deploy-aca.yaml` and update input descriptions for clarity.
    
    * Update `apps-aca.yaml` image reference to `smerek:3`.
    
    * Refactor `primary.yaml` workflow: enable `push` trigger, simplify Terraform setup, and correct `ARM_USE_OIDC` type.
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: mwierzchowski <mwierzchowski@users.noreply.github.com>

--- primary/gha-tfdeploy.tf
+++ primary/gha-tfdeploy.tf
@@ -1,9 +1,8 @@
 locals {
   deploy_github_org  = "mwierzchowski"
   deploy_github_repo = "mono-deploy"
-  # TODO replace branch with main
-  # deploy_github_ref  = "refs/heads/main"
-  deploy_github_ref  = "refs/heads/deploy-azure"
+  deploy_github_ref_main = "refs/heads/main"
+  deploy_github_ref_feature  = "refs/heads/aca-workflow"
 }
 
 # App registration & SP used by mono-deploy to run Terraform applies
@@ -18,10 +17,27 @@ resource "azuread_service_principal" "gha_tfdeploy" {
 # Federated credential for mono-deploy main branch
 resource "azuread_application_federated_identity_credential" "gha_tfdeploy_main" {
   application_id = azuread_application.gha_tfdeploy.id
-  display_name   = "gha-${local.deploy_github_repo}-${replace(local.deploy_github_ref, "/", "-")}"
+  display_name   = "gha-${local.deploy_github_repo}-${replace(local.deploy_github_ref_main, "/", "-")}"
   issuer         = "https://token.actions.githubusercontent.com"
-  subject        = "repo:${local.deploy_github_org}/${local.deploy_github_repo}:ref:${local.deploy_github_ref}"
+  subject        = "repo:${local.deploy_github_org}/${local.deploy_github_repo}:ref:${local.deploy_github_ref_main}"
   audiences      = ["api://AzureADTokenExchange"]
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [display_name]
+  }
+}
+
+# Federated credential for mono-deploy feature branch
+resource "azuread_application_federated_identity_credential" "gha_tfdeploy_feature" {
+  application_id = azuread_application.gha_tfdeploy.id
+  display_name   = "gha-${local.deploy_github_repo}-${replace(local.deploy_github_ref_feature, "/", "-")}"
+  issuer         = "https://token.actions.githubusercontent.com"
+  subject        = "repo:${local.deploy_github_org}/${local.deploy_github_repo}:ref:${local.deploy_github_ref_feature}"
+  audiences      = ["api://AzureADTokenExchange"]
+  lifecycle {
+    prevent_destroy = true
+    ignore_changes  = [display_name]
+  }
 }
 
 # Least-privilege scope for Terraform: the environment RG

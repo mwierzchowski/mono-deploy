615e477a3eab2da6966ab312b6ee69498a344626
Marcin Wierzchowski <mwierzch@gmail.com>
2025-10-19 17:47:13 +0200

    cleanup

--- bootstrap/gha.tf
+++ bootstrap/gha.tf
@@ -18,18 +18,6 @@ resource "azuread_application_federated_identity_credential" "gha_env_cd_terrafo
   audiences      = ["api://AzureADTokenExchange"]
 }
 
-resource "azurerm_role_assignment" "gha_contributor" {
-  scope                = data.azurerm_subscription.current.id
-  role_definition_name = "Contributor"
-  principal_id         = azuread_service_principal.gha_cd_terraform.id
-}
-
-resource "azurerm_role_assignment" "gha_blob_contrib" {
-  scope                = azurerm_storage_account.tfstate.id
-  role_definition_name = "Storage Blob Data Contributor"
-  principal_id         = azuread_service_principal.gha_cd_terraform.id
-}
-
 output "cd_terraform_github_secrets" {
   value = {
     AZURE_CLIENT_ID       = azuread_application.gha_cd_terraform.client_id

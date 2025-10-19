8a505c0901fa8d0a2742047884394010e7b2fa13
Marcin Wierzchowski <mwierzch@gmail.com>
2025-10-19 15:43:58 +0200

    add blob contributor role

--- devops/packages.tf
+++ devops/packages.tf
@@ -4,6 +4,17 @@ resource "azurerm_storage_container" "packages" {
   container_access_type = "private"
 }
 
+data "azuread_service_principal" "gha_ci_artifacts" {
+  display_name = "gha-mono-ci-artifacts"
+}
+
+# Allow GitHub Actions (ci-artifacts) to push/pull blobs
+resource "azurerm_role_assignment" "gha_ci_artifacts_blob_contrib" {
+  scope                = azurerm_storage_container.packages.resource_manager_id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = data.azuread_service_principal.gha_ci_artifacts.id
+}
+
 output "packages_base_url" {
   value = "https://${azurerm_storage_account.devops.name}.blob.core.windows.net/${azurerm_storage_container.packages.name}"
 }

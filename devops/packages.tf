f8249c642c06d4a1772f3a3fab6eab2772dfd803
Marcin Wierzchowski <mwierzch@gmail.com>
2025-10-20 19:30:11 +0200

    Add dev packages cleaning

--- devops/packages.tf
+++ devops/packages.tf
@@ -4,20 +4,23 @@ resource "azurerm_storage_container" "packages" {
   container_access_type = "private"
 }
 
-resource "azurerm_storage_management_policy" "packages_cleanup" {
+resource "azurerm_storage_management_policy" "devops_storage_policy" {
   storage_account_id = azurerm_storage_account.devops.id
 
   rule {
     name    = "delete-dev-after-1-day"
     enabled = true
+
     filters {
-      blob_types = ["blockBlob"]
+      blob_types  = ["blockBlob"]
       prefix_match = ["packages/"]
-      blob_index_match {
-        name  = "stage"
-        value = "dev"
+      match_blob_index_tag {
+        name      = "stage"
+        operation = "Equals"
+        value     = "dev"
       }
     }
+
     actions {
       base_blob {
         delete_after_days_since_modification_greater_than = 1
